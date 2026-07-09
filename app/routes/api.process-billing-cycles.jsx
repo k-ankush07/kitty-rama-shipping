4ee9ce077d99a9bb2512edd33963d7079b6617a9
k-ankush07 <ankush@ecombithub.com>
2026-07-09 19:03:28 +0530

    update cart features

--- app/routes/api.process-billing-cycles.jsx
+++ app/routes/api.process-billing-cycles.jsx
@@ -2,7 +2,6 @@ import { unauthenticated } from "../shopify.server";
 import prisma from "../db.server"; 
 const EXTRA_SETTINGS_NAMESPACE = "subscription_app";
 
-
 function metaKeyForGroup(groupId) {
   const numericId = groupId.split("/").pop();
   return `extra_settings_${numericId}`;
