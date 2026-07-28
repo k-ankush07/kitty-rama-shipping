b43f8bddba3097e9132e20eced55fa540187c2ab
k-ankush07 <ankush@ecombithub.com>
2026-07-28 17:25:14 +0530

    Initial commit

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -15,10 +15,7 @@ export default function App() {
 
   return (
     <AppProvider embedded apiKey={apiKey}>
-      <s-app-nav>
-        <s-link href="/app">Home</s-link>
-      </s-app-nav>
-      <Outlet />
+    
     </AppProvider>
   );
 }
